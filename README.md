ba5d066e299ad37e988cdcd6c9a098d74f4f9599
Winston Kotzan <winston.kotzan@moserit.com>
2024-02-04 15:39:37 -0500

    Switch to tab delimited format

--- README.md
+++ README.md
@@ -6,11 +6,15 @@ A script for running the Ookla Speedetest via command line and outputting the re
 
 Install the Ookla Speedtest CLI tool: https://www.speedtest.net/apps/cli
 
-Install the following line in your `/etc/crontab`, replacing <MYUSERACCOUNT> and /path/to:
-0 */4 * * * <MYUSERACCOUNT> /path/to/speedtest-auto/run-speedtest.sh >> ~/cron_log.txt 2>&1
-
 ## Running
 
 1. Run the script `./run-speedtest.sh`. It will run the Ruby program that performs the speedtest
 2. It will run the Speedtest against the servers in ./data/server-list.txt
 3. Results will be put in the CSV file ./data/speedtest-output.csv
+
+## To Run as cron job
+
+Install the following line in your `/etc/crontab`, replacing <MYUSERACCOUNT> and /path/to:
+`0 */4 * * * <MYUSERACCOUNT> /path/to/speedtest-auto/run-speedtest.sh >> ~/cron_log.txt 2>&1`
+
+Make sure that you `chmod +x` both `run-speedtest.sh` and `speedtest.rb`
--- speedtest.rb
+++ speedtest.rb
@@ -5,6 +5,15 @@ require 'time'
 
 current_file_path = File.expand_path(__FILE__)
 CURRENT_DIR = File.dirname(current_file_path)
+OUTPUT_FILE = File.join(CURRENT_DIR, 'data', 'speedtest-output.csv')
+SERVER_LIST_FILE = File.join(CURRENT_DIR, 'data', 'server-list.txt')
+
+def create_output_file
+  return if File.exists?(OUTPUT_FILE)
+
+  headers = "Time\tServer\tPing\tDownload\tUpload\n"
+  File.open(OUTPUT_FILE, 'w') { |f| f.write(headers) }
+end
 
 def run_speedtest(server)
   return if server[0] == '#'
@@ -21,13 +30,14 @@ def run_speedtest(server)
     (result.dig('download', 'bandwidth').to_f * 8 / 1_000_000).round(2),
     (result.dig('upload', 'bandwidth').to_f * 8 / 1_000_000).round(2),
   ]
-  line = "#{values.join('|')}\n"
+  line = "#{values.join("\t")}\n"
   puts "Result: #{line}"
 
-  File.open(File.join(CURRENT_DIR, 'data', 'speedtest-output.csv'), 'a').write(line)
+  File.open(OUTPUT_FILE, 'a') { |f| f.write(line) }
 end
 
+create_output_file
 
-servers = File.open(File.join(CURRENT_DIR, 'data', 'server-list.txt')).read.split("\n")
+servers = File.open(SERVER_LIST_FILE).read.split("\n")
 
 servers.each { |server| run_speedtest(server) }
