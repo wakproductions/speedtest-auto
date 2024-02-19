f812f99589adbc026b980c1bf02f61c8ab2ad8bb
Winston Kotzan <winston.kotzan@moserit.com>
2024-02-18 20:54:02 -0500

    Add IP Address to output

--- speedtest.rb
+++ speedtest.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'pry'
+require 'net/http'
 require 'json'
 require 'time'
 
@@ -11,24 +13,30 @@ SERVER_LIST_FILE = File.join(CURRENT_DIR, 'data', 'server-list.txt')
 def create_output_file
   return if File.exists?(OUTPUT_FILE)
 
-  headers = "Time\tServer\tPing\tDownload\tUpload\n"
+  headers = "Time\tServer\tPing\tDownload\tUpload\tMy IP Address\n"
   File.open(OUTPUT_FILE, 'w') { |f| f.write(headers) }
 end
 
-def run_speedtest(server)
+def get_ip_address
+  url = URI('https://api.ipify.org')
+  Net::HTTP.get(url)
+end
+
+def run_speedtest(server, ip_address: '')
   return if server[0] == '#'
 
   puts "Running Speedtest - #{server}"
   json_result = `speedtest -s #{server} -f json`
   result = JSON.parse(json_result)
 
-  # Time,Server,Ping,Download,Upload
+  # Time,Server,Ping,Download,Upload,IP Address
   values = [
     Time.now.utc.iso8601,
     "#{result.dig('server', 'location')} (#{result.dig('server', 'name')})",
     result.dig('ping', 'latency'),
     (result.dig('download', 'bandwidth').to_f * 8 / 1_000_000).round(2),
     (result.dig('upload', 'bandwidth').to_f * 8 / 1_000_000).round(2),
+    ip_address
   ]
   line = "#{values.join("\t")}\n"
   puts "Result: #{line}"
@@ -38,6 +46,7 @@ end
 
 create_output_file
 
+ip_address = get_ip_address
 servers = File.open(SERVER_LIST_FILE).read.split("\n")
 
-servers.each { |server| run_speedtest(server) }
+servers.each { |server| run_speedtest(server, ip_address:) }
